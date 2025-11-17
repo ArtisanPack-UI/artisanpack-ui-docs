18878243fb99085dd0823a3aee788ca7cb933bb1
Jacob Martella <viewfromthebox94@gmail.com>
2025-11-17 17:32:04 -0600

    Fixed something for the deployment

---
+++ bootstrap/cache/modules.php
@@ -0,0 +1,23 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Admin\\Providers\\AdminServiceProvider',
+    1 => 'Modules\\Auth\\Providers\\AuthServiceProvider',
+    2 => 'Modules\\Core\\Providers\\CoreServiceProvider',
+    3 => 'Modules\\Packages\\Providers\\PackagesServiceProvider',
+    4 => 'Modules\\Pages\\Providers\\PagesServiceProvider',
+    5 => 'Modules\\Users\\Providers\\UsersServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Admin\\Providers\\AdminServiceProvider',
+    1 => 'Modules\\Auth\\Providers\\AuthServiceProvider',
+    2 => 'Modules\\Core\\Providers\\CoreServiceProvider',
+    3 => 'Modules\\Packages\\Providers\\PackagesServiceProvider',
+    4 => 'Modules\\Pages\\Providers\\PagesServiceProvider',
+    5 => 'Modules\\Users\\Providers\\UsersServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,214 @@
+<?php return array (
+  'artisanpack-ui/accessibility' => 
+  array (
+    'aliases' => 
+    array (
+      'A11y' => 'ArtisanPack\\Accessibility\\Laravel\\Facades\\A11y',
+    ),
+    'providers' => 
+    array (
+      0 => 'ArtisanPack\\Accessibility\\Laravel\\A11yServiceProvider',
+    ),
+  ),
+  'artisanpack-ui/core' => 
+  array (
+    'aliases' => 
+    array (
+      'Core' => 'ArtisanPackUI\\Core\\Facades\\Core',
+    ),
+    'providers' => 
+    array (
+      0 => 'ArtisanPackUI\\Core\\CoreServiceProvider',
+    ),
+  ),
+  'artisanpack-ui/icons' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'ArtisanPackUI\\Icons\\IconsServiceProvider',
+    ),
+  ),
+  'artisanpack-ui/livewire-ui-components' => 
+  array (
+    'aliases' => 
+    array (
+      'LivewireUiComponents' => 'ArtisanPack\\LivewireUiComponents\\Facades\\LivewireUiComponents',
+    ),
+    'providers' => 
+    array (
+      0 => 'ArtisanPack\\LivewireUiComponents\\LivewireUiComponentsServiceProvider',
+    ),
+  ),
+  'artisanpack-ui/security' => 
+  array (
+    'aliases' => 
+    array (
+      'Security' => 'ArtisanPackUI\\Security\\Facades\\Security',
+    ),
+    'providers' => 
+    array (
+      0 => 'ArtisanPackUI\\Security\\SecurityServiceProvider',
+    ),
+  ),
+  'blade-ui-kit/blade-heroicons' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BladeUI\\Heroicons\\BladeHeroiconsServiceProvider',
+    ),
+  ),
+  'blade-ui-kit/blade-icons' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BladeUI\\Icons\\BladeIconsServiceProvider',
+    ),
+  ),
+  'laravel/boost' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Boost\\BoostServiceProvider',
+    ),
+  ),
+  'laravel/mcp' => 
+  array (
+    'aliases' => 
+    array (
+      'Mcp' => 'Laravel\\Mcp\\Server\\Facades\\Mcp',
+    ),
+    'providers' => 
+    array (
+      0 => 'Laravel\\Mcp\\Server\\McpServiceProvider',
+    ),
+  ),
+  'laravel/pail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Pail\\PailServiceProvider',
+    ),
+  ),
+  'laravel/roster' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Roster\\RosterServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'livewire/flux' => 
+  array (
+    'aliases' => 
+    array (
+      'Flux' => 'Flux\\Flux',
+    ),
+    'providers' => 
+    array (
+      0 => 'Flux\\FluxServiceProvider',
+    ),
+  ),
+  'livewire/livewire' => 
+  array (
+    'aliases' => 
+    array (
+      'Livewire' => 'Livewire\\Livewire',
+    ),
+    'providers' => 
+    array (
+      0 => 'Livewire\\LivewireServiceProvider',
+    ),
+  ),
+  'livewire/volt' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Livewire\\Volt\\VoltServiceProvider',
+    ),
+  ),
+  'mhmiton/laravel-modules-livewire' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Mhmiton\\LaravelModulesLivewire\\LaravelModulesLivewireServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'nwidart/laravel-modules' => 
+  array (
+    'aliases' => 
+    array (
+      'Module' => 'Nwidart\\Modules\\Facades\\Module',
+    ),
+    'providers' => 
+    array (
+      0 => 'Nwidart\\Modules\\LaravelModulesServiceProvider',
+    ),
+  ),
+  'owenvoke/blade-fontawesome' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'OwenVoke\\BladeFontAwesome\\BladeFontAwesomeServiceProvider',
+    ),
+  ),
+  'pestphp/pest-plugin-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Pest\\Laravel\\PestServiceProvider',
+    ),
+  ),
+  'spatie/laravel-markdown' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelMarkdown\\MarkdownServiceProvider',
+    ),
+  ),
+  'tormjens/eventy' => 
+  array (
+    'aliases' => 
+    array (
+      'Eventy' => 'TorMorten\\Eventy\\Facades\\Events',
+    ),
+    'providers' => 
+    array (
+      0 => 'TorMorten\\Eventy\\EventServiceProvider',
+      1 => 'TorMorten\\Eventy\\EventBladeServiceProvider',
+    ),
+  ),
+);
